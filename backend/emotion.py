21717a6e2b96f6c198b8b059a0312b8dbb733f8f
Aayush-Th <thakuraayush633@gmail.com>
2025-10-02 15:40:09 +0530

    Adding emotion

--- backend/emotion.py
+++ backend/emotion.py
@@ -1,2 +1,8 @@
+from base64 import b64encode, b64decode
 from deepface import DeepFace
-emotion = DeepFace.analyse(image_path,action=['emotion'])
+
+emotion = ["Angry", "Disgust", "Fear", "Happy", "Sad", "Surprise", "Neutral"]
+
+
+
+emotion = DeepFace.analyse(image_path, action=['emotion'])
