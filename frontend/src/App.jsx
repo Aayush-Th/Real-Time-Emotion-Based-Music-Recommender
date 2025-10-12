0b26758954e7d798f7db310ffa3ac94de1d4699c
Aayush-Th <thakuraayush633@gmail.com>
2025-10-12 23:22:56 +0530

    controls

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import navbar from './components/navbar';
 import main from './components/main';
 import footer from './components/footer';
+import control from './components/control';
+import Loader from './components/Loader';
 
 const App = () => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
       <navbar />
+      <control />
+      <Loader />
       <main />
       <footer />
     </div>
